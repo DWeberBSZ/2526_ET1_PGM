d2202622783c4b2493b7cc8de24b3489769da376
Dominik Weber <d.weber@berufsschule.com>
2025-09-30 12:27:02 +0200

    Einführung, Hello World

--- 00_Einfuehrung_Hello_World/Hello_World.py
+++ 00_Einfuehrung_Hello_World/Hello_World.py
@@ -1,36 +1,23 @@
-""" 
-1. Konsole öffnen
-2. Mit Befehl cd ins Verzeichnis der Datei wechseln.
-3. Mit Befehl dir Inhalt des Verzeichnis auflisten.
-4. Mit python [Dateiname] Programm ausführen.
-"""
+# Das ist ein Kommentar!
 # Buch Kapitel 1.5
 # - Wir schreiben unser erstes Programm!
 # - Python arbeitet ein Programm von OBEN nach UNTEN und Zeile für Zeile ab!
 # - Ein Python-Programm nennt man auch SKRIPT
-
 # - In Python programmiert man mit ANWEISUNGEN
 # - Eine Anweisung ist in der Regel eine Zeile Programmcode
-
 # - Wir schreiben Anweisungen, um etwas auf dem Ausgabekanal (der "Konsole") auszugeben:
-print("Hello World") 
-print("Das ist mein erstes Python-Skript")
+print(5) # Das ist eine Anweisung
+print(7) # Das ist die zweite Anweisung
+print("Hello World") # Strg+C (Kopieren), Strg+V (Einfügen)
+print('Hello World') # Einfache ' gehen auch
 print(5+5)
-print(5**2)
-print("Hallo " + "hier " + "bin ich")
-print("Hallo"*2)
-input("Das ist eine Eingabeaufforderung")
+print(5+5**2)
+print("Hello" + " " + "World") # Zusammenhängen von ZEICHENKETTEN mit +
+print("Hello"*5)
+print(5/3)
+print(5//3) # Ganzahl-Division
+print(5%3) # Ergebnis 2: Rest einer Ganzahl-Division (Modulo)
 
-# Das ist ein Kommentar! Dieser wird nicht ausgewertet!
-
-# VARIABLE: Platzhalter für Informationen
-# 1. Beliebiger Variablenname
-rufname = "Dominik Weber" 
-# <---------------------
-# Information wird von rechts nach links kopiert
-# = Zuweisungsoperator
-
-print(rufname)
-print("Dominik Weber")
-
+name = input("Wie ist dein Name?")
+print(name)
 
