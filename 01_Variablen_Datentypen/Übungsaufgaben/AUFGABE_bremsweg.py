d3d8af0e3e9f7815eca48a29209ee90aee2605c0
Dominik Weber <d.weber@berufsschule.com>
2025-10-07 14:17:15 +0200

    weitere Übungsaufgaben zu Variablen und Datentypen

---
+++ 01_Variablen_Datentypen/Übungsaufgaben/AUFGABE_bremsweg.py
@@ -0,0 +1,19 @@
+"""
+Aufgabe: Bremsweg
+====================
+
+In der Fahrschule lernt man folgende Faustformeln zur Berechnung von Anhaltewegen:
+
+Reaktionsweg (in m) = (Geschwindigkeit (km/h) / 10) * 3
+Bremsweg (in m)     = (Geschwindigkeit (km/h) / 10) * (Geschwindigkeit (km/h) / 10)
+Anhalteweg (in m)   = Reaktionsweg + Bremsweg
+
+AUFGABENSTELLUNG:
+=================
+- Lesen Sie die Geschwindigkeit in km/h mit der input-Anweisung von der Konsole ein.
+- Führen Sie die Berechnung des Anhaltewegs mit geeigneten Variablen und Zuweisungen durch.
+- Geben Sie das Ergebnis des Anhaltewegs auf der Konsole aus.
+- Kontrollieren Sie das Ergebnis mit einem Taschenrechner.
+
+Zeit: 15 Minuten, Einzelarbeit oder Partnerarbeit
+"""
---
+++ 01_Variablen_Datentypen/Übungsaufgaben/ZUSATZAUFGABE_zinsrechner.py
@@ -0,0 +1,31 @@
+""" 
+Aufgabe: Zinsrechner
+====================
+
+- Ziel der Aufgabe ist die Entwicklung eines Zinsrechners.
+- Der Zinsrechner soll nach folgender Formel funktionieren:
+
+    Endkapital = Startkapital * Rendite^Laufzeit
+    
+- Die Laufzeit wird in Jahren angegeben und beschränkt sich auf ganze Zahlen.
+
+AUFGABENSTELLUNG:
+=================
+Schreiben Sie ein Python-Skript, das die Werte für das Startkapital, die Rendite (in Prozent)
+und die Laufzeit in Jahren von der Konsole einliest und in Variablen speichert.
+
+Verwenden Sie zur Kontrolle für die Berechnung des Endkapitals die folgenden Werte:
+    - Startkapital in €: 100€
+    - Rendite in %: 10%
+    - Laufzeit: 7 Jahre
+Das Endkapital beträgt mit diesen Werten 194.87 €.
+
+Geben Sie das Endkapital in folgender Form auf der Konsole aus:
+"Ihr Endkapital beträgt 194.87€."
+
+TIPP:
+    Für die Berechnung mit der Formel muss die Rendite in Prozent auf 100% bzw. 1 addiert werden,
+    also z.B. 1.1 für eine Rendite von 10%.
+
+Zeit: 20 Minuten, Einzelarbeit oder Partnerarbeit
+"""
