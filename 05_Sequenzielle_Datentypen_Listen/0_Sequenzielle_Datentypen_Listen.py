f7948a75db5d1312cbdc88c82178e08674d50f7a
Dominik Weber <d.weber@berufsschule.com>
2025-12-09 08:36:06 +0100

    Wiederholung zu Listen

--- 05_Sequenzielle_Datentypen_Listen/0_Sequenzielle_Datentypen_Listen.py
+++ 05_Sequenzielle_Datentypen_Listen/0_Sequenzielle_Datentypen_Listen.py
@@ -13,23 +13,23 @@
 ╚═════════════════════════════════════╝
 """ 
 # Einen sequenziellen Datentypen (Sequenz) kennen wir schon:
-txt = "Hello World" # Das ist ein sequenzeller Datentyp!!!
+#txt = "Hello World" # Das ist ein sequenzeller Datentyp!!!
 
 # Mithilfe einer FOR-SCHLEIFE kann man durch SEQUENZEN durchlaufen.
-for buchstabe in txt:
-    print(buchstabe)
+#for buchstabe in txt:
+#    print(buchstabe)
 
 # Auch die range-Funktion liefert eine SEQUENZ:
 # range(10) -> 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
     
 # Weitere Beispiele mit Strings:
-txt = "Hello \nWorld"           # Escapezeichen, Sonderzeichen, \n ist Zeilenumbruch
-txt = "Hello World \\ World"    # Mit \ werden Sonderzeichen eingeleitet
-txt = R"Hello \nWorld"          # raw-String -> keine Interpretation von Sonderzeichen!!!
+#txt = "Hello \nWorld"           # Escapezeichen, Sonderzeichen, \n ist Zeilenumbruch
+#txt = "Hello World \\ World"    # Mit \ werden Sonderzeichen eingeleitet
+#txt = R"Hello \nWorld"          # raw-String -> keine Interpretation von Sonderzeichen!!!
 
-print(txt.lower())              # String wird hierbei nicht verändert, müsste in einer separaten Variable abgelegt werden!
-print(txt.upper())
-print(txt)          
+#print(txt.lower())              # String wird hierbei nicht verändert, müsste in einer separaten Variable abgelegt werden!
+#print(txt.upper())
+#print(txt)          
 
 """ 
 ╔═════════════════════════════════════╗
@@ -41,20 +41,38 @@ print(txt)
 # Eine LISTE sind mehrere Variablen "hintereinander".
 # Definition einer Liste durch die Verwendung von ECKIGEN KLAMMERN.
 
-x = [3, 99, "Ein Text"]     # In einer Liste können beliebige Datentypen stehen!
+meine_liste = [] # Definition einer leeren Liste
+meine_liste = list()
 
-# FRAGE: Wie viele Elemente hat diese Liste?
-y = [42, 65, [45, 89], 88]  # ANTWORT: Die Liste hat vier Elemente. Das dritte Elemente ist eine weitere Liste.
+eine_nicht_leere_liste = [1, 2, 3, 4, 5, 6, 7]
 
-# LISTEN ANPASSEN.
-# Mit append() können Elemente an eine Liste NACH der Definition angehängt werden.
+eine_gemischte_liste = [1, True, "Hallo"] # GEMISCHTE
+das_ist_eine_verschachtelte_liste = [1, True, [1, 2, 3, 4]] # VERSCHACHTELTE
 
-print(x)
-x.append(12)    # WICHTIG: Die Liste wird hierbei verändert! (Bei den obigen Stringfunktionen war das nicht der Fall!)
-print(x)
+print(f"Länge meiner gemischten Liste: {len(eine_gemischte_liste)}")
+print(f"Länge meiner verschachtelten Liste: {len(das_ist_eine_verschachtelte_liste)}")
 
-# Tupel: wie Listen, allerdings UNVERÄNDERBAR!!! Runde klammern, statt eckige Klammern!
-tupel = (3, 99, "Ein Text") # Nach Definition nicht mehr veränderbar!
-tupel = 3, 99, "Ein Text"   # Geht auch ohne Klammern, ist das gleiche wie oben!!!
+eine_nicht_leere_liste.append(8) # Damit kann man EINZELNE Elemente zur Liste hinzufügen
+eine_nicht_leere_liste.append(9)
+eine_nicht_leere_liste.append(10)
 
-# Deswegen Vorischt bei Fließkommadefinitionen: a = 3,5 <- das ist ein Tupel, keine Kommazahl mehr!
+for x in range(11, 15):
+    eine_nicht_leere_liste.append(x)
+    
+    
+liste_1 = [1, 2, 3, 4]
+liste_2 = ["Hallo", "mein Name", "ist", "Weber"]
+
+zusammengefügte_liste = liste_1 + liste_2
+
+liste_1.extend(liste_2) # So kann eine Liste um mehrere Elemente erweitert werden
+zusammengefügte_liste = liste_1
+
+print(zusammengefügte_liste)
+
+# Es gibt auch "Listen", die man nachträglich nicht mehr verändern kann:
+# TUPEL
+
+das_ist_ein_tupel = (1, 2, 3) # Append oder extend funktioniert hier nicht
+
+
