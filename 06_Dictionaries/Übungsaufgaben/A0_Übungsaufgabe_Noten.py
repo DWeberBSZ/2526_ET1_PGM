a686e1daf23857f3249176e3e0a461fd4dd32420
Dominik Weber <d.weber@berufsschule.com>
2026-01-13 15:22:05 +0100

    Unterricht zu Dictionaries

---
+++ 06_Dictionaries/Übungsaufgaben/A0_Übungsaufgabe_Noten.py
@@ -0,0 +1,31 @@
+'''
+Übungsaufgabe: Durchschnittspunktzahl mit Dictionary berechnen.
+================================================================
+
+*** INFORMIEREN: ***
+1. Durchlaufen von Dictionaries: Weber erklärt's!
+ 
+2. Für die Bearbeitung der Aufgabe informieren Sie sich zunächst, 
+wie Einträge zu einem Dictionary hinzugefügt werden können:
+https://www.python-kurs.eu/python3_dictionaries.php (nur Kapitel "Einführung")
+
+Gegeben ist ein Dictionary mit Schülern und den jeweiligen 
+Punktzahlen ihrer Prüfungen.
+
+Schreiben Sie ein Programm, das die Durchschnittspunktzahl 
+für jeden Schüler berechnet und in einem neuen Dictionary abspeichert.
+
+Geben Sie anschließend für jeden Schüler den Namen und die zugehörige 
+Durchschnittspunktzahl auf der Konsole aus.
+
+HINWEIS: Greifen Sie für die Berechnung des Durchschnitts auf die
+Funktionen sum() und len() zurück.
+'''
+
+# Das Dictionary mit Schülern und ihren Noten
+noten = {
+    'Max': [85, 90, 92],
+    'Anna': [78, 85, 80],
+    'Tom': [90, 88, 94],
+    'Lisa': [82, 75, 88]
+}
---
+++ 06_Dictionaries/Übungsaufgaben/A1_Übungsaufgabe_Wörterbuch.py
@@ -0,0 +1,69 @@
+'''
+Übungsaufgabe: Wörterbuch
+=========================
+
+* ZIEL: 
+===================
+Ziel der Aufgabe ist die Programmierung eines vereinfachten Deutsch-Englisch-Wörterbuchs als Python-Skript mithilfe des Datentyps 'Dictionary'.
+Das Skript soll ein deutsches Wort von der Konsole einlesen und in das entsprechende englische Wort übersetzen. 
+Das übersetzte englische Wort soll auf der Konsole ausgegeben werden.
+
+* AUFGABENSTELLUNG:
+===================
+Programmieren Sie ein Python-Skript, welches mithilfe der input-Funktion ein deutsches Wort als Zeichenkette von der Konsole einliest und
+mithilfe eines Dictionaries auf Englisch übersetzt.
+
+Beschränken Sie sich hierbei auf folgende deutsche Wörter und deren englische Übersetzungen:
+
+"Rot" -> "Red"
+"Grün" -> "Green"
+"Gelb" -> "Yellow"
+"Blau" -> "Blue"
+
+Prüfen Sie nach der Eingabe des deutschen Worts zunächst, ob das deutsche Wort im Dictionary als Schlüssel hinterlegt ist. 
+Falls ja, geben Sie das übersetzte Wort auf der Konsole mit der print-Funktion in einfacher Form aus.
+Falls nein, geben Sie mit der print-Funktion einen Hinweis auf der Konsole aus, dass für dieses Wort keine Übersetzung hinterlegt ist.
+
+Definieren Sie für das Dictionary eine Variable mit dem Namen 'de_en'. 
+
+Hinweis: Nach der Definition des Dictionary müssen keine zusätzlichen Schlüssel-Werte-Paare mehr hinzugefügt werden können.
+
+ZUSATZAUFGABE: 
+==============
+
+Definieren Sie zusätzlich ein Wörterbuch mit dem Namen 'en_fr' für die Übersetzung von Englisch nach Französisch.
+Erweitern Sie das bestehende Programm, dass es mithilfe der Kombinierung der zwei Dictionaries 'de_en' und 'en_fr' 
+die eingegebenen deutschen Wörter in französische Wörter übersetzt und auf der Konsole ausgibt. 
+Verzichten Sie dabei auf die Verwendung eines Dictionaries zur direkten Übersetzung von Deutsch nach Französisch.
+
+
+* HILFSMITTEL:
+==============
+    - Diskussion  mit Nachbarin / Nachbar, maximal 3 Personen zusammen, am Platz sitzen bleiben
+    - Beliebige Dokumentation
+    - Lehrkraft, bei Fragen melden: Ich komme zum Platz
+
+* ZEIT: 
+=======
+20 Minuten, 
+'''
+
+# 1. Definition des Dictionary mit vorgegebenen Schlüssel-Werte-Paaren für die Übersetzung.
+'''
+"Rot" -> "Red"
+"Grün" -> "Green"
+"Gelb" -> "Yellow"
+"Blau" -> "Blue"
+'''
+
+
+# 2. Einlesen eines deutschen Wortes von der Konsole mit input-Funktion.
+
+
+
+# 3. Falls das deutsche Wort im Dictionary als Schlüssel hinterlegt ist: Übersetztes Wort mit print-Funktion ausgeben.
+# Überprüfen, ob Schlüssel enthalten ist: if "key" in "dict".
+
+
+
+# 4. Falls nicht: Hinweis auf der Konsole mit print-Funktion ausgeben, dass für dieses Wort keine Übersetzung hinterlegt ist.
