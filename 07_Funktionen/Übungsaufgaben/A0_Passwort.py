8f5a5bcfba03ca85628272092e364e284150c9ad
Dominik Weber <d.weber@berufsschule.com>
2026-01-27 14:35:38 +0100

    Übungsaufgabe zu Funktionen

---
+++ 07_Funktionen/Übungsaufgaben/A0_Passwort.py
@@ -0,0 +1,29 @@
+"""
+Aufgabe: Qualität von Passwörtern bewerten
+==========================================
+
+Schreiben Sie eine Funktion, welche die Qualität von Passwörtern nach einem einfachen Punktesystem bewertet.
+
+Das Passwort soll von der Konsole eingelesen werden und als Parameter der Funktion übergeben werden.
+
+Die Qualität des Passworts soll dem Benutzer auf der Konsole mitgeteilt werden.
+
+Es gelten folgende Regeln:
+
+- Passwort mit 7 oder weniger Zeichen: immer 0 Punkte
+- ab 8 Zeichen: +1 Punkt
+- enthält sowohl Groß- als auch Kleinschreibung: +1 Punkt
+- enthält mehr als sechs unterschiedliche Zeichen: +1 Punkt
+- enthält zumindest eine Ziffer: +1 Punkt
+- enthält zumindest ein Sonderzeichen: +1 Punkt
+
+QUELLE: Fachbuch, Michael Kofler "Python, der Grundkurs"
+
+Zeit: 20 Minuten, Einzelarbeit.
+"""
+
+
+    
+
+    
+        
