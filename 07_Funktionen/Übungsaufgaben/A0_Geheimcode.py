4888d8730fe06c075befb705a3e9ef9397789b3f
Dominik Weber <d.weber@berufsschule.com>
2026-01-27 15:14:12 +0100

    Übungsaufgaben zu Funktionen 2.o

---
+++ 07_Funktionen/Übungsaufgaben/A0_Geheimcode.py
@@ -0,0 +1,10 @@
+"""
+Aufgabe: Geheimcode
+===================
+
+Schreiben Sie eine Funktion, die einen Text aufnimmt und mit einer Caesar-Chiffre verschlüsselt. 
+
+Dies ist eine der einfachsten und am häufigsten bekannten Verschlüsselungstechniken. 
+
+Jeder Buchstabe im Text wird durch einen Buchstaben ersetzt, der eine feste Anzahl von Stellen weiter im Alphabet enthält.
+"""
